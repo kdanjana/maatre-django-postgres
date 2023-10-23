716205b452f994b4c269e48d4901edb32dc7fa4a
anjana <kad.an.devi@gmail.com>
2023-10-23 09:49:29 +0530

    added urlpatterns

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 
 from django.conf import settings
@@ -32,6 +33,11 @@ urlpatterns = [
     path("payment/", include("payment.urls")),      #payment app
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += (
+        re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    )
