ecfee5800698624e61206c68484e96ce56584bcf
anjana <kad.an.devi@gmail.com>
2023-11-04 14:17:42 +0530

    added env

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG'))
+DEBUG = bool(os.environ.get('DEBUG', default=0))
 #DEBUG = False
 
 ALLOWED_HOSTS = ['*']
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+
+set -e
+# We'll check if database is ready to connect or not
+echo "Waiting for database..."
+echo DB_NAME: ${DB_NAME}
+echo DB_HOST: ${DB_HOST}
+echo DB_PORT: ${PORT}
+while ! nc -z ${DB_HOST} ${PORT}; do sleep 1; done
+echo "Connected to database."
+
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-gunicorn ecommerce.wsgi:application -c gunicorn_config.py
+gunicorn ecommerce.wsgi:application -c gunicorn_config.py --log-level=debug --access-logfile=- --error-logfile=-
